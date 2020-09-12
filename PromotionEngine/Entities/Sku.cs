4524d02417a2c99de57c7556744cca160dbc7047
Sunny Chauhan <chauhansunny9429@gmail.com>
2020-09-12 12:45:27 +0530

    Added Sku entity class

---
+++ PromotionEngine/Entities/Sku.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Entities
+{
+    public class Sku
+    {
+        #region Private members
+        private readonly char _name;
+        private readonly int _price;
+        private bool _isProcessed;
+        #endregion
+        public Sku(char name, int price, bool isProcessed = false)
+        {
+            _name = name;
+            _price = price;
+            _isProcessed = isProcessed;
+
+        }
+
+        public char Name => _name;
+        public int Price => _price;
+        public bool isProcessed
+        {
+            get
+            {
+                return _isProcessed;
+            }
+            set
+            {
+                _isProcessed = value;
+            }
+        }
+    }
+}
