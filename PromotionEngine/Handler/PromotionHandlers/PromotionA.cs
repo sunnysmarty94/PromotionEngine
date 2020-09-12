42b7b53c4722d7d5f42b1db83e474a824ac6d672
Sunny Chauhan <chauhansunny9429@gmail.com>
2020-09-12 13:54:05 +0530

    Added Promotion Handler for A Sku

---
+++ PromotionEngine/Handler/PromotionHandlers/PromotionA.cs
@@ -0,0 +1,36 @@
+﻿using PromotionEngine.Entities;
+using PromotionEngine.Handler.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngine.Handler.PromotionHandlers
+{
+    public class PromotionA : IReceiver<Cart>
+    {
+        public int Handle(ref Cart cart)
+        {
+            int value = 0;
+            var unprocessedA = cart.Items
+                                .Where(x => x.Name == 'A' && x.isProcessed == false)
+                                .ToList();
+            var loop = unprocessedA.Count / 3;
+            var skip = 0;
+            for (var i = 0; i <= loop; i++)
+            {
+                var processingList = unprocessedA.Skip(skip).Take(3);
+                if (processingList.Count() == 3)
+                {
+                    foreach (var p in processingList)
+                    {
+                        p.isProcessed = true;
+                    }
+                    value += 130;
+                }
+                skip += 3;
+            }
+            return value;
+        }
+    }
+}
